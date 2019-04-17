addfb53373f5f020ce95c637c7a8042e5be5498b
Rodrigo <rcoto@protonmail.com>
2019-04-16 21:07:55 -0700

    HTML/CSS: Responsive nagivation bar completed it

--- src/app.js
+++ src/app.js
@@ -1,2 +1,46 @@
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
+
+// Navigation
+const navButton = document.querySelector(".navigation__button");
+const navContent = document.querySelector(".navigation__content");
+const navNav = document.querySelector(".navigation__nav");
+const navList = document.querySelector(".navigation__list");
+const navLink = document.querySelector(".navigation__link");
+
+// if ( $(window).width() < 514)
+
+console.log(window.innerWidth);
+console.log(window.outerWidth);
+
+// Will add the class hide to navigation__nav
+window.innerWidth <= 1200 && navNav.classList.add("hide");
+
+navButton.addEventListener("click", () => {
+  // Apply all active classes.
+  const contentClass = "content-mobile-active";
+  const navClass = "nav-mobile-active";
+  const listClass = "list-mobile-active";
+  const linkClass = "link-mobile-active";
+  const hideClass = "hide";
+
+  navContent.classList.contains(contentClass)
+    ? navContent.classList.remove(contentClass)
+    : navContent.classList.add(contentClass);
+
+  navNav.classList.contains(navClass)
+    ? navNav.classList.remove(navClass)
+    : navNav.classList.add(navClass);
+
+  navNav.classList.contains(hideClass)
+    ? navNav.classList.remove(hideClass)
+    : navNav.classList.add(hideClass);
+
+  navList.classList.contains(listClass)
+    ? navList.classList.remove(listClass)
+    : navList.classList.add(listClass);
+
+  navLink.classList.contains(linkClass)
+    ? navLink.classList.remove(linkClass)
+    : navLink.classList.add(linkClass);
+});
